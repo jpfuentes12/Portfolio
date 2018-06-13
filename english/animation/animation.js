133e907d899f939ad9efff8eeac3884add0e18e2
jpfuentes12 <39918360+jpfuentes12@users.noreply.github.com>
2018-06-13 18:58:55 +0200

    Add files via upload

---
+++ english/animation/animation.js
@@ -0,0 +1,20 @@
+var menu = document.getElementById("menu");
+var menu_cont = document.getElementById("menu_cont");
+var menu_father = document.getElementById("menu_father");
+var menu_check = false;
+menu.onclick = function(){
+    if(menu_check == false){
+   menu_cont.style.transform= "translateY(970px)";
+
+    menu_cont.style.transition = "0.5s";
+            menu_cont.style.opacity = "1";
+    menu_check = true;
+        
+    } else{
+    menu_cont.style.transform= "translate(0px)";
+ 
+    menu_cont.style.transition = "0.5s";
+           menu_cont.style.opacity = "0";
+    menu_check = false;
+    }
+}
